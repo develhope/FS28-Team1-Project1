3642791b60ddc86f4a2e80702cb69bc13d55cd52
Franklin <nwaguugonna@gmail.com>
2024-05-01 18:02:35 +0200

    first commit

--- Team1.js
+++ Team1.js
@@ -1,5 +1,18 @@
 const team1 = [
   {
+        name: "Franklin",
+        surname: "Nwagu",
+        age: 21,
+        city: "Roma",
+        hobby: "Playing football",
+        favoriteFood: "Carbonara",
+        favoriteVideoGame: "League of Legends",
+        favoriteFilm: "Avengers infity war",
+        favoriteBook: "Harry Potter",
+        petName: "blacky",
+      },
+    ];
     
-  }
-]
+   
+  
+
