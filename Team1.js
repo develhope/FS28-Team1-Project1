80e51b873af04ef58e882b0ceddc6f998bdb47f9
fiamma ferrone <fiammaferrone149@gmail.com>
2024-05-01 17:53:05 +0200

    Team 1 - Fiamma

--- Team1.js
+++ Team1.js
@@ -1,5 +1,16 @@
 const team1 = [
   {
-    
+    name: "Fiamma",
+    surname: "Ferrone",
+    age: 19,
+    city: "Roma",
+    hobby: "Musica",
+    favoriteFood: "Amatriciana",
+    favoriteVideoGame: " ",
+    favoriteFilm: "Spirit Cavallo Selvaggio",
+    favoriteBook: "1984",
+    petName: "Nessie",
+
   }
 ]
+
