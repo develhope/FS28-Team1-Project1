aac08021a16d30f4e555f9e9083201ac96eb0007
unknown <adamofall23@gmail.com>
2024-05-01 17:45:12 +0200

    First commit

--- Team1.js
+++ Team1.js
@@ -1,5 +1,14 @@
 const team1 = [
   {
-    
+    name:"Adamo",
+    surname:"Fall",
+    age:31,
+    city:"Este",
+    hobby:"Calcio",
+    favoriteFood:"Pizza",
+    favoriteVideoGame:"Fifa",
+    favoriteFilm:"Batman",
+    favoriteBook:"Harry Potter",
+    petName:"Puppy",
   }
 ]
