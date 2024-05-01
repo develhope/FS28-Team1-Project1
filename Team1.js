779a9b92880ff3a2167f2c19bc2dc9655c9a5834
Giovanni-Val <Gio95.vit@gmai.com>
2024-05-01 17:27:26 +0200

    team1 Giovanni

--- Team1.js
+++ Team1.js
@@ -1,5 +1,14 @@
 const team1 = [
   {
-    
+    name: "Giovanni",
+    surname: "Vitolo",
+    age: 29
+    city: "Eboli",
+    hobby: "",
+    favoriteFood: "Cannelloni",
+    favoriteVideoGame: "Final Fantasy X",
+    favoriteFilm: "Shutter Island",
+    favoriteBook: "",
+    petName: "Sasha",
   }
 ]
